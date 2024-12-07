2788b49dc4e777d9767f0c2ed8d0772baec6ac5e
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-07 15:34:06 +0100

    test if choices works

--- commands/utility/randomImageGenerator.js
+++ commands/utility/randomImageGenerator.js
@@ -1,21 +1,36 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const { AttachmentBuilder } = require('discord.js');
+const { InteractionContextType } = require('discord.js');
+const { PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rimage') //everything with small letter
-		.setDescription('Responds with a random image'),
+		.setDescription('Responds with a random image')
+		.addTagsOption(option =>
+			option.setName('tags')
+                .setDescription('Search of the tags that are in the image generator')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Pulbic', value: 'public' },
+					{ name: 'sfw', value: 'SFW' },
+					{ name: 'Functional', value: 'Functional' },
+					{ name: 'Source', value: 'Source' },
+				)),
 
 	async execute(interaction) {
-		const image = `https://pic.re/image?random=${Date.now()}`; //in Typescript it was needed to be `` instead of ''
+        const tags = interaction.options.getString('tags');
+		const image1 = `https://pic.re/image/${tags}`;
+		const image2 = `?random=${Date.now()}`; //in Typescript it was needed to be `` instead of ''
+		const image = image1 + image2;
 
 		const Embed = new EmbedBuilder()
 			.setColor(0x0099FF)
 			.setTitle('Random Image')
 			.setImage(image)
 			.setTimestamp()
-			.setFooter({ text: 'Powered by/pic.re || sfw'});
+			.setFooter({ text: `Powered by/pic.re || ${tags}`});
 
 		await interaction.reply({ embeds: [Embed]});
 	},
