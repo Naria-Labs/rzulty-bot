f600d537635e81cca6ab6df3f2c8c0811a2ee256
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-07 16:28:41 +0100

    random rather than date

--- commands/utility/randomImageGenerator.js
+++ commands/utility/randomImageGenerator.js
@@ -20,15 +20,15 @@ module.exports = {
 				)),
 
 	async execute(interaction) {
-        const tags = interaction.options.getString('tags');
+		const tags = interaction.options.getString('tags');
 		const image1 = `https://pic.re/image/${tags}`;
-		//const image2 = `?random=${Date.now()}`; //in Typescript it was needed to be `` instead of ''
-		//const image = image1 + image2;
+		const image2 = '?random'; //in Typescript it was needed to be `` instead of ''
+		const image = image1 + image2;
 
 		const Embed = new EmbedBuilder()
 			.setColor(0x0099FF)
 			.setTitle('Random Image')
-			.setImage(image1)
+			.setImage(image)
 			.setTimestamp()
 			.setFooter({ text: `Powered by/pic.re || ${tags}`});
 
