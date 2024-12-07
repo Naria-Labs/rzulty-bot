a192d32941e54773dbe84317e05155b74cc6572d
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-07 19:26:41 +0100

    test if it works

--- commands/utility/randomImageGenerator.js
+++ commands/utility/randomImageGenerator.js
@@ -7,26 +7,57 @@ const { PermissionFlagsBits } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('rimage') //everything with small letter
-		.setDescription('Responds with a random image')
-		.addStringOption(option =>
-			option.setName('tags')
-                .setDescription('Search of the tags that are in the image generator (all are sfw)')
-				.setRequired(true)
-				.addChoices(
-					{ name: 'waifu', value: 'waifu' },
-					{ name: 'neko', value: 'neko' },
-					{ name: 'dance', value: 'dance' },
-					{ name: 'wink', value: 'wink' },
-				)),
-	
+		.setDescription('Responds with a random waifu image')
+		.addSubcommandGroup((group) =>
+			group
+				.setName('sfw')
+				.setDescription('SFW Images')
+				.addSubcommand((subcommand) =>
+					subcommand
+						.setName('tags')
+						.setDescription('Search of the tags that are in the image generator')
+						.addStringOption(option =>
+							option.setName('tags')
+								.setDescription('Search of the tags that are in the image generator (all are sfw)')
+								.addChoices(
+									{ name: 'waifu', value: 'waifu' },
+									{ name: 'neko', value: 'neko' },
+									{ name: 'dance', value: 'dance' },
+									{ name: 'wink', value: 'wink' },
+								)
+								.setRequired(true)
+								
+						)
 
-
-
+			),
+		)
+		.addSubcommandGroup((group) =>
+			group
+				.setName('nsfw')
+				.setDescription('NSFW Images')
+				.addSubcommand((subcommand) =>
+					subcommand
+						.setName('tags')
+						.setDescription('Search of the tags that are in the image generator')
+						.addStringOption(option =>
+							option.setName('tags')
+								.setDescription('Search of the tags that are in the image generator (all are sfw)')
+								.addChoices(
+									{ name: 'waifu', value: 'waifu' },
+									{ name: 'neko', value: 'neko' },
+									{ name: 'trap', value: 'trap' },
+									{ name: 'bj', value: 'blowjob' },
+								)
+								.setRequired(true)
+								
+						)
+			),
+		),
 
 	async execute(interaction) {
 
 		const tags = interaction.options.getString('tags');
-		const response = await fetch(`https://api.waifu.pics/sfw/${tags}`);
+		const response = await fetch(`https://api.waifu.pics/${horny}/${tags}`);
 		const parseData = await response.json();
 		const image = parseData.url;
 
