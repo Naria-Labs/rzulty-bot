b5c8d7906441cda96daeeeb5e11fe43275dd469a
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-07 17:17:18 +0100

    changed api

--- commands/utility/randomImageGenerator.js
+++ commands/utility/randomImageGenerator.js
@@ -10,27 +10,32 @@ module.exports = {
 		.setDescription('Responds with a random image')
 		.addStringOption(option =>
 			option.setName('tags')
-                .setDescription('Search of the tags that are in the image generator')
+                .setDescription('Search of the tags that are in the image generator (all are sfw)')
 				.setRequired(true)
 				.addChoices(
-					{ name: 'Public', value: 'public' },
-					{ name: 'sfw', value: 'SFW' },
-					{ name: 'Functional', value: 'Functional' },
-					{ name: 'Source', value: 'Source' },
+					{ name: 'waifu', value: 'waifu' },
+					{ name: 'neko', value: 'neko' },
+					{ name: 'dance', value: 'dance' },
+					{ name: 'wink', value: 'wink' },
 				)),
+	
+
+
+
 
 	async execute(interaction) {
+
 		const tags = interaction.options.getString('tags');
-		const image1 = `https://pic.re/image/${tags}`;
-		const image2 = '?random'; //in Typescript it was needed to be `` instead of ''
-		const image = image1 + image2;
+		const image1 = `https://api.waifu.pics/sfw/${tags}`;
+		const parseData = await fetch(image1);
+		const image = parseData.url
 
 		const Embed = new EmbedBuilder()
 			.setColor(0x0099FF)
 			.setTitle('Random Image')
 			.setImage(image)
 			.setTimestamp()
-			.setFooter({ text: `Powered by/pic.re || ${tags}`});
+			.setFooter({ text: `Powered api.waifu.pics || ${tags}`});
 
 		await interaction.reply({ embeds: [Embed]});
 	},
