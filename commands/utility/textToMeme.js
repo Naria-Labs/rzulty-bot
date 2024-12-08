52344ea57ac7bc05fad96221d2fa490ae3140175
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-08 13:21:18 +0100

    added next command textToMeme

---
+++ commands/utility/textToMeme.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('mmessage')
+		.setDescription('Make your message more fun or cringe')
+		.addStringOption(option =>
+			option.setName('emote')
+				.setDescription('text to emotes')),
+
+
+	async execute(interaction) {
+		const emote = interaction.options.getString('emote');
+		const str = emote.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+		const text = str.split('');
+		for (let i = 0; i < text.length; i++) {
+			text[i] = text[i].toLowerCase();
+			if (text[i] === ' ' || ',' || '.')
+				i++;
+        }
+		for (let i = 0; i < text.length; i++) {
+            text[i] = `:${text[i]}:`;
+		}
+		let arrayString = text.join('');
+		await interaction.reply(`${arrayString}`);
+	},
+};
