31fc06745ca5476569b79d43af4d6ed853e02290
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-08 11:56:20 +0100

    test if emotes work

---
+++ commands/utility/memeMessage.js
@@ -0,0 +1,37 @@
+﻿const { SlashCommandBuilder } = require('discord.js');
+
+const emote = interaction.options.getString();
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('memeMessage')
+		.setDescription('Make your message more fun or cringe')
+		.addStringOption(option =>
+			option.setName('emote')
+				.setDescription('text to emotes')),
+
+
+	async execute(interaction) {
+        
+		await interaction.reply('Pong!');
+	},
+};
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('memeMessageRandom')
+		.setDescription('Make your message more fun or cringe')
+		.addStringOption(option =>
+			option.setName('randomEmote')
+				.setDescription('random emote')),
+
+
+	async execute(interaction) {
+		const emote = interaction.options.getString('randomEmote');
+		const meme = ["🔥🔥🔥💯💯💯🗣️🗣️🗣️", "💀💀💀", "🔥🔥🔥", "🗣️🗣️🗣️🔥🔥🔥🔥🔥🔥", "🙏🙏😭😭", "💯💯💯", "☝️", "🙏🙏🗣🗣🗣🔊🔊🔊🔊🔊"];
+		const number = meme.length;
+		const random = Math.floor(Math.random() * length);
+
+		await interaction.reply(`${emote} ${meme[random]}`);
+	},
+};
