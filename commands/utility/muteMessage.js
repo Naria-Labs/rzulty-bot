82e75036a18a4bd2323ad89056a1278870ba9397
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-11 00:41:36 +0100

    without await ?

--- commands/utility/muteMessage.js
+++ commands/utility/muteMessage.js
@@ -39,7 +39,7 @@ module.exports = {
 		}
 		setTimeout(unmute, `${time}` * 1000);
 		function unmute() {
-			await userMentioned.voice.setMute(false, 'You will get away from saying it. For now');
+			userMentioned.voice.setMute(false, 'You will get away from saying it. For now');
 		}
 
 	},
