1cad76c3181cc1f6009d2af6b5ce49535b44811f
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-10 23:08:04 +0100

    next test

--- commands/utility/muteMessage.js
+++ commands/utility/muteMessage.js
@@ -3,10 +3,15 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('serverdeafen')
-		.setDescription('Server mute for set ammount of time'),
+		.setDescription('Server mute for set ammount of time')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('Find a user that you wanna mute')
+				.setRequired(true)
+		),
 
-	async execute(interaction) {
-		const userMentioned = interaction.options.getMember('user');
-		await userMentioned.setMute(true, 'You did fucky wacky');
+	execute(interaction) {
+		const userMentioned = interaction.options.getUser('user');
+		userMentioned.setMute(true, 'You did fucky wacky');
 	},
 };
