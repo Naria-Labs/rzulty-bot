7a8894387b8ed96c9cca02853f8499b1594721ce
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-20 11:07:33 +0100

    test

--- commands/utility/embedInfo.js
+++ commands/utility/embedInfo.js
@@ -13,23 +13,24 @@ module.exports = {
 		const author = interaction.member;
 		const userMentioned = interaction.options.getMember('user');
 		const userID = interaction.member.id;
+		const userMentionedMember = interaction.option.getUsers('user');
 		const userCreated = userMentioned.createdAt;
-		const userTag = userMentioned.tag;
-		const userHuman = userMentioned.bot;
-		const userAccentCol = userMentioned.hexAccentColor;
-		const accentCol = userMentioned.accentColor;
-		const col = userMentioned.displayColor;
+		const userTag = userMentionedMember.tag;
+		const userHuman = userMentionedMember.bot;
+		const userAccentCol = userMentionedMember.hexAccentColor;
+		const accentCol = userMentionedMember.accentColor;
+		const col = userMentionedMember.displayColor;
 		const thumbnail = userMentioned.displayAvatarURL();
 		//const userBanner = userMentioned.bannerURL({ dynamic: true});
 		const smth = "'"
 
 		const avatarEmbed = new EmbedBuilder()
 			.setColor(0x003253) //f you discord
-			.setTitle(`User ${userMentioned} profile`)
+			.setTitle('User profile')
 			.setThumbnail(`${thumbnail}`)
 			.setDescription(`User ${userMentioned} profile with other infromation about it`)
 			.addFields(
-				{ name: 'Name', value: `${userMentioned}`, inline: true},
+				{ name: 'Name', value: `<@${userMentioned}>`, inline: true},
 				{ name: 'Account created', value: `${userCreated}`, inline: true},
 				{ name: 'Tag', value: `${userTag}`, inline: true},
 				{ name: 'Bot or not', value: `${userHuman}` },
@@ -40,6 +41,6 @@ module.exports = {
 			.setTimestamp()
 			.setFooter({ text:`That${smth}s all folks!`, iconURL: 'https://i.ytimg.com/vi/AYZz_qYw_j4/maxresdefault.jpg'});
 
-		await interaction.fetchReply({ embeds: [avatarEmbed]});
+		await interaction.reply({ embeds: [avatarEmbed]});
 	},
 };
