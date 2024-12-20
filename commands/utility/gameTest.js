636b734ade4d76f80583ff85a8bacf401cca408b
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-20 18:00:00 +0100

    test

--- commands/utility/gameTest.js
+++ commands/utility/gameTest.js
@@ -1,96 +1,98 @@
 ﻿const { Client, ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 
-
-
-
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('startgame')
-		.setDescription('Starts the game')
-		.addIntegerOption(option =>
-			option.setName('number')
-				.setDescription('Set the board')
-				.setMinValue(1)
-				.setMaxValue(8)),
+    data: new SlashCommandBuilder()
+        .setName('startgame')
+        .setDescription('Starts the game')
+        .addIntegerOption(option =>
+            option.setName('number')
+                .setDescription('Set the board')
+                .setMinValue(1)
+                .setMaxValue(8)
+        ),
 
+    async execute(interaction) {
+        const boardSize = interaction.options.getInteger('number');
+        let Board = Array.from({ length: boardSize }, () => Array(boardSize).fill('<:space:1315336436987203716>'));
 
-	async execute(interaction) {
+        let playerX = Math.floor(Math.random() * boardSize);
+        let playerY = Math.floor(Math.random() * boardSize);
+        Board[playerX][playerY] = '<a:ASGawrA:755990103473651803>';
 
-		const board = interaction.options.getInteger('number');
-		const Board = Array.from({ length: board }, () => Array(board).fill('<:space:1315336436987203716>'));
-		const cordx = Math.floor(Math.random() * board);
-		const cordy = Math.floor(Math.random() * board);
-		Board[cordx][cordy] = '<a:ASGawrA:755990103473651803>';
-		const left = new ButtonBuilder()
-			.setCustomId('left')
-			.setStyle(ButtonStyle.Primary)
-			.setEmoji('⬅')
-			.setDisabled(false);
-		const up = new ButtonBuilder()
-			.setCustomId('up')
-			.setStyle(ButtonStyle.Primary)
-			.setEmoji('⬆')
-			.setDisabled(false);
-		const down = new ButtonBuilder()
-			.setCustomId('down')
-			.setStyle(ButtonStyle.Primary)
-			.setEmoji('⬇️')
-			.setDisabled(false);
-		const right = new ButtonBuilder()
-			.setCustomId('right')
-			.setStyle(ButtonStyle.Primary)
-			.setEmoji('➡️')
-			.setDisabled(false);
+        const getBoardString = () => Board.map(row => row.join('')).join('\n');
 
+        const left = new ButtonBuilder()
+            .setCustomId('left')
+            .setStyle(ButtonStyle.Primary)
+            .setEmoji('⬅');
+
+        const up = new ButtonBuilder()
+            .setCustomId('up')
+            .setStyle(ButtonStyle.Primary)
+            .setEmoji('⬆');
+
+        const down = new ButtonBuilder()
+            .setCustomId('down')
+            .setStyle(ButtonStyle.Primary)
+            .setEmoji('⬇️');
+
+        const right = new ButtonBuilder()
+            .setCustomId('right')
+            .setStyle(ButtonStyle.Primary)
+            .setEmoji('➡️');
 
         const row = new ActionRowBuilder()
             .addComponents(left, up, down, right);
 
-        const BoardToString = Board.map(row => row.join('')).join('\n');
-
-		if (interaction.ActionRowBuilder === 'left' && cordy - 1 >= 0) {
-			Board[cordx][cordy] = '<:space:1315336436987203716>';
-			Board[cordx][cordy - 1] = '<a:ASGawrA:755990103473651803>';
-			interaction.editReply({
-				BoardToString = Board.map(row => row.join('')).join('\n')
-            });
-		} else {
-            interaction.ActionRowBuilder.setDisabled(true);
-		}
-		if (interaction.ActionRowBuilder === 'up' && cordx - 1 >= 0 ) {
-            Board[cordx][cordy] = '<:space:1315336436987203716>';
-			Board[cordx - 1][cordy] = '<a:ASGawrA:755990103473651803>';
-			interaction.editReply({
-				BoardToString = Board.map(row => row.join('')).join('\n')
-			});
-		} else {
-			interaction.ActionRowBuilder.setDisabled(true);
-		}
-		if (interaction.ActionRowBuilder === 'down' && cordx + 1 <= cordx) {
-            Board[cordx][cordy] = '<:space:1315336436987203716>';
-			Board[cordx + 1][cordy] = '<a:ASGawrA:755990103473651803>';
-			interaction.editReply({
-				BoardToString = Board.map(row => row.join('')).join('\n')
-			});
-		} else {
-			interaction.ActionRowBuilder.setDisabled(true);
-		}
-		if (interaction.ActionRowBuilder === 'right' && cordy + 1 <= cordy) {
-            Board[cordx][cordy] = '<:space:1315336436987203716>';
-			Board[cordx][cordy + 1] = '<a:ASGawrA:755990103473651803>';
-			interaction.editReply({
-				BoardToString = Board.map(row => row.join('')).join('\n')
-			});
-		} else {
-			interaction.ActionRowBuilder.setDisabled(true);
-		}
-
-
-
-		await interaction.reply({
-			content: BoardToString,
+        await interaction.reply({
+            content: getBoardString(),
             components: [row],
-		});
+        });
 
-	},
+        const collector = interaction.channel.createMessageComponentCollector({ time: 60000 });
+
+        collector.on('collect', async buttonInteraction => {
+            if (buttonInteraction.user.id !== interaction.user.id) {
+                return buttonInteraction.reply({ content: 'This game is not for you!', ephemeral: true });
+            }
+
+            Board[playerX][playerY] = '<:space:1315336436987203716>';
+
+            switch (buttonInteraction.customId) {
+                case 'left':
+                    if (playerY > 0) playerY--;
+                    break;
+                case 'up':
+                    if (playerX > 0) playerX--;
+                    break;
+                case 'down':
+                    if (playerX < boardSize - 1) playerX++;
+                    break;
+                case 'right':
+                    if (playerY < boardSize - 1) playerY++;
+                    break;
+                default:
+                    break;
+            }
+
+            Board[playerX][playerY] = '<a:ASGawrA:755990103473651803>'; 
+
+            await buttonInteraction.update({
+                content: getBoardString(),
+                components: [row],
+            });
+        });
+
+        collector.on('end', () => {
+            left.setDisabled(true);
+            up.setDisabled(true);
+            down.setDisabled(true);
+            right.setDisabled(true);
+
+            interaction.editReply({
+                content: 'Game over!',
+                components: [row],
+            });
+        });
+    },
 };
