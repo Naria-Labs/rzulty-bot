e66cb63beab7d47175f3c92964ba307873c51175
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-20 16:25:43 +0100

    ActionRowBuilder added

--- commands/utility/gameTest.js
+++ commands/utility/gameTest.js
@@ -1,4 +1,4 @@
-﻿const { ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
+﻿const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 
 
 
