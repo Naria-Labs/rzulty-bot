bf5580d147aa105d73a541ed817a05d063b69444
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2024-12-20 16:15:59 +0100

    test if array and buttons works

---
+++ commands/utility/gameTest.js
@@ -0,0 +1,51 @@
+﻿const { ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
+
+
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('startgame')
+		.setDescription('Starts the game')
+		.addIntegerOption(option =>
+			option.setName('number')
+				.setDescription('Set the board')
+				.setMinValue(1)
+				.setMaxValue(6)),
+
+
+	async execute(interaction) {
+		const board = interaction.options.getInteger('number'),
+
+		const row = board;
+		const column = board;
+		const Board = Array.from({ lenght: rows }, () => Array(cols).fill('<:space:1315336436987203716>'));
+		const left = new ButtonBuilder()
+			.setCustomId('primary')
+			.setLabel('left')
+			.setStyle(ButtonStyle.Primary)
+			.setEmoji('⬅️');
+		const up = new ButtonBuilder()
+			.setCustomId('primary')
+			.setLabel('up')
+			.setStyle(ButtonStyle.Primary)
+			.setEmoji('⬆️');
+		const down = new ButtonBuilder()
+			.setCustomId('primary')
+			.setLabel('down')
+			.setStyle(ButtonStyle.Primary)
+			.setEmoji('⬇️');
+		const right = new ButtonBuilder()
+			.setCustomId('primary')
+			.setLabel('right')
+			.setStyle(ButtonStyle.Primary)
+			.setEmoji('➡️');
+        const row = new ActionRowBuilder()
+            .addComponents(left, up, down, right);
+
+		await interaction.reply({
+			content: [board],
+            components: [row],
+		});
+	},
+};
